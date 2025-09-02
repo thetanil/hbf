680449fb76512580d28e11da3c2195762420b1d2
Theta Nil <20186755+thetanil@users.noreply.github.com>
2025-09-02 18:04:36 +0000

    clean

--- bazel/oci/install_packages.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -o errexit -o nounset -o pipefail
-
-apt-get update && apt-get install -y --no-install-recommends \
-    curl \
-    ca-certificates \
-    && apt-get clean && rm -rf /var/lib/apt/lists/*
