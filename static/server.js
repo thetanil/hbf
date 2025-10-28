43a8782e332639ca1671a25846565cf30ee105fe
Your Name <your.email@example.com>
2025-10-28 19:07:59 +0000

    server mod

--- static/server.js
+++ static/server.js
@@ -11,10 +11,10 @@ var app = {
         if (req.path === '/') {
             res.status(200);
             res.send('<html><body><h1>HBF Server</h1><p>Available Routes:</p><ul>' +
-                     '<li>GET /hello - Hello World JSON</li>' +
-                     '<li>GET /user/:id - User info with parameter</li>' +
-                     '<li>GET /echo - Echo request info</li>' +
-                     '</ul></body></html>');
+                '<li>GET /hello - Hello World JSON again</li>' +
+                '<li>GET /user/:id - User info with parameter</li>' +
+                '<li>GET /echo - Echo request info</li>' +
+                '</ul></body></html>');
             return;
         }
 
